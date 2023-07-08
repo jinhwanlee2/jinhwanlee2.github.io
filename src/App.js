95ae177051cb03595c7d2c59ee92d8869be0c2c3
Jin Lee <jinhwanl0880@gmail.com>
2023-07-07 21:15:33 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Home from "./Home";
+import Portfolio from './Portfolio';
+import Education from "./Education";
+import Resume from "./Resume";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("home");
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navigation-bar">
+        <ul className="tab-bar">
+          <li
+            onClick={() => handleTabClick("home")}
+            className={activeTab === "home" ? "active home" : "home"}
+          >
+            Home
+          </li>
+          <li
+            onClick={() => handleTabClick("portfolio")}
+            className={activeTab === "portfolio" ? "active portfolio" : "portfolio"}
+          >
+            Portfolio
+          </li>
+          <li
+            onClick={() => handleTabClick("resume")}
+            className={activeTab === "resume" ? "active resume" : "resume"}
+          >
+            Resume
+          </li>
+          <li
+            onClick={() => handleTabClick("education")}
+            className={activeTab === "education" ? "active education" : "education"}
+          >
+            Education
+          </li>
+        </ul>
+        <div className="tab-content">
+          {activeTab === "home" && <Home />}
+          {activeTab === "portfolio" && <Portfolio />}
+          {activeTab === "resume" && <Resume />}
+          {activeTab === "education" && <Education />}
+        </div>
+      </div>
     </div>
   );
 }
