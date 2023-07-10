ea9d2a3262ecd9db8027e4ce30ae3ed679b19608
Jin Lee <jinhwanl0880@gmail.com>
2023-07-10 02:06:21 -0700

    update to home layout

--- src/Education.js
+++ src/Education.js
@@ -7,33 +7,36 @@ function App() {
     return (
       <div className="App">
         <header className="App-header">
-        <div className = "Logo-container">
-            <div className = "School">
-                <img src={beckman} className="Beckman-logo" alt="beckman" />
-                <p className="School-name">
-                    Beckman High School
-                </p>
-                <p className="Degree">
-                    High School Diploma
-                </p>
-                <p className="Years-attended">
-                    Years Attended: 2016-2019
-                </p>
+            <p>
+                Education
+            </p>
+            <div className = "Logo-container">
+                <div className = "School">
+                    <img src={beckman} className="Beckman-logo" alt="beckman" />
+                    <p className="School-name">
+                        Beckman High School
+                    </p>
+                    <p className="Degree">
+                        High School Diploma
+                    </p>
+                    <p className="Years-attended">
+                        Years Attended: 2016-2019
+                    </p>
+                </div>
+                
+                <div className = "School">
+                    <img src={ucsc} className="UCSC-logo" alt="ucsc" />
+                    <p className="School-name">
+                        University of California Santa Cruz
+                    </p>
+                    <p className="Degree">
+                        Bachelors of Science in Computer Science
+                    </p>
+                    <p className="Years-attended">
+                        Years Attended: 2022-2023
+                    </p>
+                </div>
             </div>
-            
-            <div className = "School">
-                <img src={ucsc} className="UCSC-logo" alt="ucsc" />
-                <p className="School-name">
-                    University of California Santa Cruz
-                </p>
-                <p className="Degree">
-                    Bachelors of Science in Computer Science
-                </p>
-                <p className="Years-attended">
-                    Years Attended: 2022-2023
-                </p>
-            </div>
-        </div>
 
 
    
